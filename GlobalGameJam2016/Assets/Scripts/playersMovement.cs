36c9ee5283c0629ff1f12922830282ce5b59d156
Alexander Moran <j-moran@optusnet.com.au>
2016-01-30 02:29:11 +1000

    Start on movement

---
+++ GlobalGameJam2016/Assets/Scripts/playersMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class playersMovement : MonoBehaviour
+{
+
+    //public
+    public float speed;
+
+    //private
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //called before physics updates -- physics should go here
+    void fixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+}
