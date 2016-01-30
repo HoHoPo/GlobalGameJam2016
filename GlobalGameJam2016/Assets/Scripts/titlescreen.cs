c9f2e2ec27aa651810801d4e8f975900e255beac
Aikynaro <aikynaro@gmail.com>
2016-01-31 01:16:33 +1000

    very ugly title screen

---
+++ GlobalGameJam2016/Assets/Scripts/titlescreen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class titlescreen : MonoBehaviour {
+    //just stealing code from playersMovement
+    public enum controls
+    {
+        keyboard,
+        joystick1,
+        joystick2,
+        joystick3,
+        joystick4
+    };
+
+    public controls PlayerControls;
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("UseCircleJoy1") || Input.GetButtonDown("UseCircleJoy2") || Input.GetButtonDown("UseCircleJoy3") || 
+            Input.GetButtonDown("UseCircleJoy4") || Input.GetButtonDown("UseCircle"))
+        {
+            Application.LoadLevel("MainScene");
+        }
+        {
+            Debug.Log("moo");
+        }
+	
+	}
+}
