a9fef47b82c70c824c50547cfa3e79797300be48
Alexander Moran <j-moran@optusnet.com.au>
2016-01-30 19:20:55 +1000

    resource pile limitations
    still needs to update player pickup

--- GlobalGameJam2016/Assets/Scripts/ResourePile.cs
+++ GlobalGameJam2016/Assets/Scripts/ResourePile.cs
@@ -1,41 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+////resource disks
+//[System.Serializable]
+//public class shirnk
+//{
+//    public float maxScale, minScale;
+//    public GameObject disk;
+
+//}
+
 public class ResourePile : MonoBehaviour {
 
     public resouceManager.resourceType type;
-	//public bool infinte = true;
-	//public double probSpawnPerSecond = 0.5;
-	//private double timeoff = 0;
+	public bool infinte = true;
+    public double respawnSeconds = 2;
+    private double timeoff = 0;
 
-	//private bool _ready = true;
-	//public bool ready
-	//{
-	//	get { return _ready; }
-	//	set
-	//	{
-	//		_ready = value;
-	//		if (value == false)
-	//		{
-	//			timeoff = Time.time;
-	//		}
-	//	}
-	//}
-	//// Use this for initialization
-	void Start () {
+    //public shirnk shirnkage;
+    //private GameObject disk;
+
+    private bool _ready = true;
+    public bool ready
+    {
+        get { return _ready; }
+        set
+        {
+            _ready = value;
+            if(infinte == true)
+            {
+                _ready = true;
+            }
+            if (value == false)
+            {
+                timeoff = Time.time;
+            }
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
 
         this.gameObject.tag = ("resource");
 	}
-	
-	// Update is called once per frame
-	//void Update () {
-	//	if( ready == false)
-	//	{
-	//		if (Random.value <(Time.time - timeoff) * probSpawnPerSecond)
-	//		{
-	//			ready = true;
-	//		}
-	//	}
-	//}
+
+    //Update is called once per frame
+
+    void Update()
+    {
+        if (ready == false)
+        {
+
+           // diskScale(Time.time - timeoff / respawnSeconds, true);
+            if ((Time.time - timeoff) > respawnSeconds)
+            {
+                ready = true;
+             //   diskScale(Time.time - timeoff / respawnSeconds, true);
+            }
+        }
+    }
+
+    resouceManager.resourceType takeResource( ref bool carry)
+    {
+        carry = ready;
+        return type;
+    }
 
 }
